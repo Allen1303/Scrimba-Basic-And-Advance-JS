dfd8a3adb0e39dad3aba7bd339b10d18d632ad88
“Allen1303” <132642677+Allen1303@users.noreply.github.com>
2024-12-01 23:43:55 -0500

    "Store save-el in variable, create count separator with dash, render to save-el using textContent"

--- Passenger_Counter_App/Practice_Environment/practice.js
+++ Passenger_Counter_App/Practice_Environment/practice.js
@@ -21,7 +21,6 @@ function lapTime() {
 lapTime();
 
 let lapsCompleted = 0;
-
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
 //SOLUTION
@@ -32,3 +31,38 @@ lapIncrementor();
 lapIncrementor();
 lapIncrementor();
 console.log(lapsCompleted);
+
+/** Dec 1st 2024 Exercise
+ * @param 
+ *  Create two variables, name and greeting. The name variable should store your name,
+and the greeting should store e.g. "Hi, my name is "
+
+Create a third variable, myGreeting, that concatenates the two strings
+ Log myGreeting to the console
+ */
+//SOLUTION
+let name = "Allen";
+let greeting = "Hi, my nsme is";
+
+let myGreeting = greeting + " " + name + ".";
+
+console.log(myGreeting);
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+console.log(4 + 5);
+console.log("2" + "4");
+console.log("5" + 1);
+console.log(100 + "100");
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+const welcomeEl = document.getElementById("welcome-el");
+
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+let welcomeName = "Allen";
+let welcomeGreeting = "Welcome back ";
+// Render the welcome message using welcomeEl.innerText
+welcomeEl.innerText = welcomeGreeting + welcomeName;
+console.log(welcomeEl.innerText +="👋🏽" );
+//////////////////////////////////////////////////////////////////////////////////////////////////
--- Passenger_Counter_App/script.js
+++ Passenger_Counter_App/script.js
@@ -7,16 +7,24 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked (log it out)
 // change the count-el in the HTML to reflect the new count
+
 const countEl = document.getElementById("count-el");
+// 1. Grab the save-el paragraph and store it in a variable called saveEl
+const saveEl = document.getElementById("save-el");
 let count = 0;
 const counter = () => {
-  count = count + 1;
-  countEl.innerText = count;
-  console.log(count);
-};
-counter();
-// 1. Create a function, save(), which logs out the count when it's called
-const save = () => {
-  console.log(count);
+  count++;
+  countEl.textContent = count;
+  // console.log(count);
 };
 
+// 1. Create a function, save(), which logs out the count when it's called
+const save = () => {
+  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+  let saveCounter = count + " - ";
+  // 3. Render the variable in the saveEl using textContent
+  saveEl.textContent += saveCounter;
+  // NB: Make sure to not delete the existing content of the paragraph
+  countEl.textContent = 0;
+  count = 0;
+};
