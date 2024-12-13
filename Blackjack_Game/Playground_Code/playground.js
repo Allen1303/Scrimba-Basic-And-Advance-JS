d2db83f8036553b004e4d5249c9627f60c15be2d
“Allen1303” <132642677+Allen1303@users.noreply.github.com>
2024-12-13 03:22:03 -0500

    "Implement game logic using variables, functions, onclick events, and CSS styling. Incorporate challenges by utilizing loops and arrays."

---
+++ Blackjack_Game/Playground_Code/playground.js
@@ -0,0 +1,79 @@
+"use strict";
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+//EXERCISE 1
+let age = 22;
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+if (age < 21) {
+  console.log("You can not enter the club!");
+} else {
+  console.log("Welcome!");
+}
+
+//EXERCISE 2
+// Check if the person is elegible for a birthday card from the King! (100)
+
+let age2 = 100;
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+if (age2 < 100) {
+  console.log("Not eligible");
+} else if (age2 === 100) {
+  console.log("Here is your birthday card from the King!");
+} else {
+  console.log("Not eligible, you have already gotten one");
+}
+//EXERCISE 3
+
+// Create an array that lists your i.e. experience, education, licenses, skills or similar
+// The items of the array should be strings
+
+let personalBio = [
+  "Design web applications using JavaScript",
+  "Bachelor of Science in Computer Science",
+  "Google UX Design Certificate",
+];
+console.log(personalBio);
+
+//EXERCISE 4
+let experience = [
+  "CEO at Scrimba",
+  "Frontend developer at Xeneta",
+  "People counter for Norstat",
+];
+
+// Make the following appear in the console:
+// Frontend developer at Xeneta
+// People counter for Norstat
+// CEO at Scrimba
+console.log(experience[1]);
+console.log(experience[2]);
+console.log(experience[0]);
+
+// 3. Log it out to check that you're doing it right
+//EXERCISE 6
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
+
+for (let ii = 10; ii < 101; ii += 10) {
+  console.log(ii);
+}
+//EXERCISE 7
+let cards = [7, 3, 9];
+
+// Create a for loop that logs out all the cards in the array
+// Use cards.length to specify how long the loop should run
+let cards = [7, 3, 9];
+
+// Create a for loop that logs out all the cards in the array
+// Use cards.length to specify how long the loop should run
+for (let cardsIndex = 0; cardsIndex < cards.length; cardsIndex++) {
+  const cardsArr = cards[cardsIndex];
+  console.log(cardsArr);
+}
---
+++ Blackjack_Game/script.js
@@ -0,0 +1,36 @@
+"use strict";
+/**Blackjack Game Code Logic and Exercises */
+// 1. Store the message-el paragraph in a variable called messageEl
+const messageEl = document.getElementById("message-el");
+const sumEl = document.querySelector("#sum-el");
+
+const cardsEl = document.getElementById("cards-el");
+let firstCard = 10;//TODO: Later to be dynamic value
+let secondCard = 11;//TODO: Later to be dynamic value
+let sum = firstCard + secondCard + 4;
+let hasBlackJack = false;
+let isAlive = true;
+
+let message = "";
+function startGame() {
+  cardsEl.textContent = "Cards " + firstCard + " " + secondCard;
+  sumEl.textContent = " Sum: " + sum;
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
+function newCard() {
+  console.log("Drawing a new card from the deck!");
+  let card = 7;//TODO: Update Later to dynamic value
+  sum += card;
+
+  startGame();  //TODO: Name Later
+}
